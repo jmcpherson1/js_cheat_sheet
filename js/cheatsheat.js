9377a7f6191c0a7b26c3816f5ae78ff0360459df
Jamasa McPherson <jamasamcpherson@Jamasas-MBP.home>
2015-08-08 18:13:50 -0400

    Inserted Variablescheat sheet

---
+++ js/cheatsheat.js
@@ -0,0 +1,28 @@
+//VARIABLES
+
+//String:
+"Hello World!"
+
+//Number: (all numbers in JS are floats)
+var x=5
+
+//Array: List (can include any data types; numbers do not need quotations)
+var week ['m', 't', 'w', 'th', 'f', 's', 'sun']
+
+//Booleans: True, False
+var isNight=true;
+
+
+
+
+//Debugging (alerts, comments, the console)
+
+//Different data types
+
+//Arrays
+
+//Testing
+
+//Logic
+
+//Functions
